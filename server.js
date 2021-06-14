70565a003a363ed4ca3377421c7d5d6ddab9affa
root <root@Mahmood.localdomain>
2021-06-14 21:46:01 +0300

    edit

--- server.js
+++ server.js
@@ -12,11 +12,7 @@ const PORT = process.env.PORT;
 
 app.get('/', // our endpoint name
  function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World') // our endpoint function response
+    res.json(data) // our endpoint function response
 })
 
-app.get('/weather-data', (req, res)=> {
-res.json(data)
-});
- 
 app.listen(PORT) // kick start the express server to work
